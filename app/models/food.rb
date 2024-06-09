4876cf60380620b894f22e86effbde6e7f6b6dda
same06 <typewreter@yahoo.co.jp>
2024-06-09 11:45:48 +0900

    food.rbアソシエーション修正

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,6 @@
 class Food < ApplicationRecord
     belongs_to :shop
     has_many :order_foods
-    has_many :order, through: :order_foods #第三モデルを指定
+    has_many :orders, through: :order_foods #第三モデルを指定
     #中間テーブルがある状態であれば、throughで繋がる。
 end
